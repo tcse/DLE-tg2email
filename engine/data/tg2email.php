a917863695bbdda4ae7c19772c0c1b60a87bdead
Vitaly V. Chuyakov <mail@tcse-cms.com>
2025-08-19 16:32:09 +0300

    v0.4.1

--- engine/data/tg2email.php
+++ engine/data/tg2email.php
@@ -5,7 +5,7 @@
  */
 $tg2emailConfig = [
     'tg2email_TOKEN' => '1234567890:abcdefghijklmnopqrstuvwxyzABCDE12345',
-    'tg2email_CHATID' => '757940529',
+    'tg2email_CHATID' => '0',
     'tg2email_bufferTime' => 2,
     'tg2email_adminEmail' => 'mail@tcse-cms.com',
     'tg2email_formatEmail'  => '0', // 0 = plain text, 1 = HTML
