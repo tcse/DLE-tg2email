12fbb12b422cb589a8d462970c73d89980f29092
Vitaly V. Chuyakov <mail@tcse-cms.com>
2025-08-21 19:26:28 +0300

    Update tg2email.php
        'tg2email_media_ttl' => 365, // Срок хранения ссылок в днях (0 = вечно)
        'tg2email_embedPhotos' => '1', // 0 = нет, 1 = да, вкладывать фото < 5 МБ

--- engine/data/tg2email.php
+++ engine/data/tg2email.php
@@ -9,4 +9,6 @@ $tg2emailConfig = [
     'tg2email_bufferTime' => 2,
     'tg2email_adminEmail' => 'mail@tcse-cms.com',
     'tg2email_formatEmail'  => '0', // 0 = plain text, 1 = HTML
+    'tg2email_media_ttl' => 365, // Срок хранения ссылок в днях (0 = вечно)
+    'tg2email_embedPhotos' => '1', // 0 = нет, 1 = да, вкладывать фото < 5 МБ
 ];
